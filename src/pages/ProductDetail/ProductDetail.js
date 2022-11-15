8b210552386df208b215d0c260539ecca3a17371
sukminwoo <stargiraffe3710@gmail.com>
2022-11-15 20:55:28 +0900

    ADD: 스토리 페이지

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -6,6 +6,22 @@ const ProductDetail = () => {
     <div className="ProductDetail">
       <div className="ProductDetail__leftbody">
         <img alt="사람이미지" src="/images/ProductDetail/person-standing.png" />
+        <img alt="눈" src="/images/ProductDetail/eye (1).png" />
+        <img
+          alt="장"
+          src="/images/ProductDetail/free-icon-large-intestine-4615532.png"
+        />
+        <img alt="간" src="/images/ProductDetail/free-icon-liver-288228.png" />
+        <img
+          alt="관절,뼈"
+          src="/images/ProductDetail/free-icon-joint-4710034.png"
+        />
+      </div>
+      <div className="ProductDetail__productmenu">
+        <div className="ProductDetail__productmenu__drug1">약1</div>
+        <div className="ProductDetail__productmenu__drug2">약1</div>
+        <div className="ProductDetail__productmenu__drug3">약1</div>
+        <div className="ProductDetail__productmenu__drug4">약1</div>
       </div>
     </div>
   );
