2ba6e030d438c428f26ff03a65f868beb8c8b03a
SangYoonLee <prestro1231@hufs.ac.kr>
2022-11-15 14:21:25 +0900

    MODIFY: 초기세팅 수정 사항 추가 반영

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from '.Router';
+import Router from './Router';
 import './styles/reset.scss';
 import './styles/common.scss';
 
