dffb35343f3a4c488cb39838707d0324d53c1a5b
sukminwoo <stargiraffe3710@gmail.com>
2022-11-15 18:04:56 +0900

    ADD: 수정중

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './ProductDetail.scss';
 
 const ProductDetail = () => {
-  return <h1>ProductDetail</h1>;
+  return (
+    <div className="ProductDetail">
+      <div className="ProductDetail__leftbody">
+        <img alt="사람이미지" src="/images/ProductDetail/person-standing.png" />
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
