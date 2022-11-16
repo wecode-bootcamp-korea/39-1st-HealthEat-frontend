a54e497946f66fff6022861de7db98699a493f34
sukminwoo <stargiraffe3710@gmail.com>
2022-11-16 17:28:15 +0900

    ADD: Nav 수정완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,35 +5,35 @@ import './Nav.scss';
 export default function Nav() {
   return (
     <header className="nav">
-      <nav className="nav__bar">
-        <div className="nav__bar__left">
-          <Link to="/store" className="nav__bar__left__store">
+      <nav className="nav-bar">
+        <div className="nav-bar-left">
+          <Link to="/store" className="nav-bar-left-store">
             <p>스토어</p>
           </Link>
         </div>
-        <div className="nav__bar__logo">
-          <Link to="/main" className="nav__bar__left__store">
-            <p>Health Eat</p>
+        <div className="nav-bar-logo">
+          <Link to="/main" className="nav-bar-left-store">
+            Health Eat
           </Link>
         </div>
-        <ul className="nav__bar__righticon">
-          <li className="nav__bar__righticon__item">
-            <Link to="/">
+        <ul className="nav-bar-righticon">
+          <li className="nav-bar-righticon-item">
+            <Link to="/cart">
               <img
                 src="/images/free-icon-add-cart-6032906.png"
                 alt="장바구니"
-                width="18px"
-                height="18px"
+                width="30px"
+                height="30px"
               />
             </Link>
           </li>
-          <li className="nav__bar__righticon__item">
+          <li className="nav-bar-righticon-item">
             <Link to="/">
               <img
                 src="images/free-icon-user-1077063.png"
                 alt="마이페이지"
-                width="15px"
-                height="15px"
+                width="25px"
+                height="25px"
               />
             </Link>
           </li>
