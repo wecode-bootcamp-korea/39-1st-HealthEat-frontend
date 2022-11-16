b491e29fdac7753e96c68d532cbdcbb8ad40a75a
SangYoonLee <prestro1231@hufs.ac.kr>
2022-11-16 18:00:44 +0900

    MODIFY: README.md 파일에 팀 내 컨벤션 추가 및 레이아웃 설정

--- src/pages/Main/PromotionSlide/PromotionSlide.js
+++ src/pages/Main/PromotionSlide/PromotionSlide.js
@@ -1,28 +1,5 @@
 import React from 'react';
 
-const sliderData = [
-  {
-    image: 'link',
-    heading: 'Slide One',
-    desc: 'This is the description of slide one',
-  },
-  {
-    image: 'link',
-    heading: 'Slide Two',
-    desc: 'This is the description of slide two',
-  },
-  {
-    image: 'link',
-    heading: 'Slide Three',
-    desc: 'This is the description of slide three',
-  },
-  {
-    image: 'link',
-    heading: 'Slide Four',
-    desc: 'This is the description of slide four',
-  },
-];
-
 const PromotionSlide = () => {
   return (
     <article className="promotion-slide">
