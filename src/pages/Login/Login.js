574f86ff7528352535dbc9b61dd5afc250beeae5
leehyewon <leehyewon959@gmail.com>
2022-11-18 14:51:44 +0900

    ADD : 로그인 페이지 레이아웃 작업 중

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import './Login.scss';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleChangeEmail = e => {
     setEmail(e.target.value);
@@ -13,27 +15,47 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  const alertMsg = () => {
+  const alertMsg = e => {
+    e.preventDefault();
     if (email.length === 0 || password.length === 0) {
       return alert('입력란을 채워주세요');
     } else {
+      login();
       return alert('로그인 성공');
     }
+  };
+
+  const login = () => {
+    fetch('http://10.58.52.124:3000/users/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        localStorage.setItem('token', data.accessToken);
+        navigate('/login');
+      });
   };
 
   return (
     <div className="login">
       <img src="./images/pill.png" alt="logo" className="logo" />
-      <form className="loginForm">
+      <form className="login-form" onSubmit={alertMsg}>
         <input
-          className="userInput"
+          className="user-input"
           type="text"
           placeholder="이메일 또는 전화번호를 입력하세요."
           title="아이디입력"
           onChange={handleChangeEmail}
         />
         <input
-          className="userInput"
+          className="user-input"
           type="password"
           placeholder="비밀번호를 입력하세요."
           minlength="5"
@@ -42,19 +64,19 @@ const Login = () => {
           onChange={handleChangePassword}
         />
 
-        <button className="btn" onClick={alertMsg}>
+        <button className="btn" type="submit">
           로그인
         </button>
       </form>
 
-      <div className="findPw">
-        <a className="pwA" href="#">
+      <div className="find-pw-body">
+        <Link className="find-pw" to="/">
           비밀번호 찾기
-        </a>
+        </Link>
         <span>|</span>
-        <a className="idA" href="#">
+        <Link className="join-user" to="/">
           회원가입
-        </a>
+        </Link>
       </div>
     </div>
   );
