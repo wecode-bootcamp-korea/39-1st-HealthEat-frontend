e0a253cb8d00ac17156bd91fbed76b77e13b90c5
SangYoonLee <prestro1231@hufs.ac.kr>
2022-11-21 18:43:40 +0900

    Refactor: 메뉴 리스트 아이콘 추가, PromotionSlide Fade 애니메이션 구현

--- src/pages/Main/MenuList/MenuList.js
+++ src/pages/Main/MenuList/MenuList.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ListItem from './ListItem/ListItem';
 
 const MENU_LIST_DATAS = [
-  { name: '눈', image: '' },
-  { name: '간', image: '' },
-  { name: '관절 / 뼈', image: '' },
-  { name: '장', image: '' },
-  { name: '다이어트', image: '' },
-  { name: '위 / 소화', image: '' },
-  { name: '피부', image: '' },
-  { name: '피로 / 활력', image: '' },
+  { name: '눈', image: '/images/mainListIcons/eye-care.png' },
+  { name: '간', image: '/images/mainListIcons/liver.png' },
+  { name: '관절 / 뼈', image: '/images/mainListIcons/articulation.png' },
+  { name: '장', image: '/images/mainListIcons/intestine.png' },
+  { name: '다이어트', image: '/images/mainListIcons/diet-food.png' },
+  { name: '위 / 소화', image: '/images/mainListIcons/stomach.png' },
+  { name: '피부', image: '/images/mainListIcons/skin-care.png' },
+  { name: '피로 / 활력', image: '/images/mainListIcons/energy-drink.png' },
 ];
 
 const MenuList = () => {
